a681a79dec9b7db6e24fa98d34c620baa880f07b
AJINKYA CHAVAN <87532290+AJINKYACHAVAN5@users.noreply.github.com>
2021-09-20 15:55:15 +0530

    script.js

---
+++ Beginner Level Task/Task 2- Music Recommondation/js/music-list.js
@@ -0,0 +1,56 @@
+// To add more song, just copy the following code and paste inside the array
+
+//   {
+//     name: "Here is the music name",
+//     artist: "Here is the artist name",
+//     img: "image name here - remember img must be in .jpg formate and it's inside the images folder of this project folder",
+//     src: "music name here - remember img must be in .mp3 formate and it's inside the songs folder of this project folder"
+//   }
+
+//paste it inside the array as more as you want music then you don't need to do any other thing
+
+let allMusic = [
+  {
+    name: "La Casa De Papel-Bella Ciao",
+    artist: "El-Professor",
+    img: "music-1",
+    src: "music-1"
+  },
+  {
+    name: "CASABLENCA",
+    artist: "Saad-Lamjarred",
+    img: "music-2",
+    src: "music-2"
+  },
+  {
+    name: "Isa",
+    artist: "Isa-Andro",
+    img: "music-3",
+    src: "music-3"
+  },
+  {
+    name: "Let Me Down Slowly",
+    artist: "Alec-Benjamin",
+    img: "music-4",
+    src: "music-4"
+  },
+  {
+    name: "On My Way",
+    artist: "Alan Walker,Sabrina Carpenter & Farruko",
+    img: "music-5",
+    src: "music-5"
+  },
+  {
+    name: "Ciao Adios",
+    artist: "Anne-Marie",
+    img: "music-6",
+    src: "music-6"
+  },
+  // like this paste it and remember to give comma after ending of this bracket }
+  // {
+  //   name: "Here is the music name",
+  //   artist: "Here is the artist name",
+  //   img: "image name here - remember img must be in .jpg formate and it's inside the images folder of this project folder",
+  //   src: "music name here - remember img must be in .mp3 formate and it's inside the songs folder of this project folder"
+  // }
+];
